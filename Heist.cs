8722b63ab4fae02a8fdeb2a667f223fb21ca92b0
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-01-15 03:14:36 -0600

    The Heist Part 1

---
+++ Heist.cs
@@ -0,0 +1,46 @@
+﻿using Heist;
+namespace TheHeist
+{
+    internal class Heist
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to The Heist");
+            var Member = new HeistMembers();
+
+            Console.Write("Please Enter Your Name: ");
+            Member.Name = Console.ReadLine();
+
+            while(String.IsNullOrWhiteSpace(Member.Name))
+            {
+                Console.WriteLine("Please Enter Name: ");
+                Member.Name = Console.ReadLine();
+            }
+
+            Console.Write("Please Enter Skill Level: ");
+            string skill = Console.ReadLine();
+            int.TryParse(skill, out int SkillLvl);
+            Member.SkillLevel= SkillLvl;
+
+            while(SkillLvl <= 0)
+            {
+                Console.WriteLine("Please enter a valid number");
+                skill = Console.ReadLine();
+                int.TryParse(skill, out SkillLvl);
+            }
+
+            Console.Write("Please Enter Courage Factor: ");
+            string CourageLvl = Console.ReadLine();
+            decimal.TryParse(CourageLvl, out decimal Courage);
+            Member.CourageFactor = Courage;
+
+            while(Courage < 0.1M || Courage > 2.0M)
+            {
+                Console.WriteLine("Please enter value between 0.1 and 2.0");
+                CourageLvl = Console.ReadLine();
+                decimal.TryParse(CourageLvl, out Courage);   
+            }
+            Console.WriteLine($"Heist Members Name: {Member.Name}, Skill Level: {Member.SkillLevel}, Courage Factor: {Member.CourageFactor}.");
+        }
+    }
+}
---
+++ HeistMembers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heist
+{
+    internal class HeistMembers
+    {
+        internal string Name { get; set; }
+        internal int SkillLevel { get; set; }
+        internal decimal CourageFactor { get; set; }
+
+    }
+}
