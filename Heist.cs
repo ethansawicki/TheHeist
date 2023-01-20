b55012265ee20c15c058ff78b58398bf0ffb43c5
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-01-20 06:57:40 -0600

    reworking some stuff but half these files i dont even know

--- Heist.cs
+++ Heist.cs
@@ -1,4 +1,4 @@
-﻿using Heist;
+﻿using TheHeist;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Metrics;
 
@@ -8,108 +8,35 @@ namespace TheHeist
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Welcome to The Heist");
-            var Member = new HeistMembers();
-            //Also This seems wayy too much theres probably an easier way
-            //First Member for Heist
-            Console.Write("Please Enter Team Member Name One: ");
-            Member.MemberNameOne = Console.ReadLine();
-            
-            while (String.IsNullOrWhiteSpace(Member.MemberNameOne))
+            Console.WriteLine("Plan your Heist");
+            Console.Write("Please enter name for member one: ");
+            string Member1Name = Console.ReadLine();
+            while (String.IsNullOrEmpty(Member1Name))
             {
-                Console.WriteLine("Please Enter A Valid Name: ");
-                Member.MemberNameOne = Console.ReadLine();
+                Console.Write("Please Enter Valid Name: ");
+                Member1Name = Console.ReadLine();
             }
             
-            Member.Members.Add(Member.MemberNameOne);
-
-            //Second Member For Heist
-            Console.Write("Please Enter Team Member Name Two: ");
-            Member.MemberNameTwo = Console.ReadLine();
-
-            while (String.IsNullOrWhiteSpace(Member.MemberNameTwo))
+            Console.Write("Please Enter Skill Level");
+            string skillLvl = Console.ReadLine();
+            int.TryParse(skillLvl, out int level);
+            while(level <= 0) 
             {
-                Console.WriteLine("Please Enter Valid Names: ");
-                Member.MemberNameTwo = Console.ReadLine();
+                Console.Write("Please enter a valid number");
+                int.TryParse(skillLvl, out level);
             }
-            Member.Members.Add(Member.MemberNameTwo);
-
-            //Third Member For Heist
-            Console.Write("Please Enter Team Member Name Three: ");
-            Member.MemberNameThree = Console.ReadLine();
-
-            while (String.IsNullOrWhiteSpace(Member.MemberNameThree))
-            {
-                Console.WriteLine("Please Enter Valid Names: ");
-                Member.MemberNameThree = Console.ReadLine();
-            };
-            Member.Members.Add(Member.MemberNameThree);
-
-            //Skill Level Input One
-            Console.Write("Please Enter Skill Level For Member One: ");
-            Member.MemberOneSkillLevel = Console.ReadLine();
-            int.TryParse(Member.MemberOneSkillLevel, out int SkillLvlOne);
-            
-
-            while(SkillLvlOne <= 0)
-            {
-                Console.WriteLine("Please enter a valid number");
-                Member.MemberOneSkillLevel = Console.ReadLine();
-                int.TryParse(Member.MemberOneSkillLevel, out SkillLvlOne);  
-            }
-            Member.SkillLevels.Add(SkillLvlOne);
-
-            //Skill Level Input Two
-            Console.Write("Please Enter Skill Level For Member Two: ");
-            Member.MemberTwoSkillLevel = Console.ReadLine();
-            int.TryParse(Member.MemberTwoSkillLevel, out int SkillLvlTwo);
-
-
-            while (SkillLvlTwo <= 0)
-            {
-                Console.WriteLine("Please enter a valid number");
-                Member.MemberTwoSkillLevel = Console.ReadLine();
-                int.TryParse(Member.MemberTwoSkillLevel, out SkillLvlTwo);
-            }
-            Member.SkillLevels.Add(SkillLvlTwo);
-
-            //Skill Level Input Three
-            Console.Write("Please Enter Skill Level For Member One: ");
-            Member.MemberThreeSkillLevel = Console.ReadLine();
-            int.TryParse(Member.MemberThreeSkillLevel, out int SkillLvlThree);
-
-
-            while (SkillLvlThree <= 0)
-            {
-                Console.WriteLine("Please enter a valid number");
-                Member.MemberThreeSkillLevel = Console.ReadLine();
-                int.TryParse(Member.MemberThreeSkillLevel, out SkillLvlThree);
-            }
-            Member.SkillLevels.Add(SkillLvlThree);
-
             Console.Write("Please Enter Courage Factor: ");
-            string CourageLvl = Console.ReadLine();
-            decimal.TryParse(CourageLvl, out decimal Courage);
-            
-
-            while(Courage < 0.1M || Courage > 2.0M)
+            string courageFactor = Console.ReadLine();
+            decimal.TryParse(courageFactor, out decimal Factor);
+            while(Factor < 0.1M || Factor > 2.0M)
             {
-                Console.WriteLine("Please enter value between 0.1 and 2.0");
-                CourageLvl = Console.ReadLine();
-                decimal.TryParse(CourageLvl, out Courage);   
+                Console.Write("Please enter valid values between 0.0 and 2.0");
+                courageFactor = Console.ReadLine();
+                decimal.TryParse(courageFactor, out Factor);
             }
-            Member.CourageLevels.Add(Courage);
-
-            foreach (string Names in Member.Members)
-            {
-                foreach (int Levels in Member.SkillLevels)
-                {
-                    foreach (decimal CourageLevel in Member.CourageLevels)
-                    {
-                        Console.WriteLine($"Heist Members Name: {Names}, Skill Level: {Levels}, Courage Factor: {CourageLevel}.");
-                    }
-                }
-            }
+            var HeistMemberOne = new Member1(Member1Name, level, Factor);
+            Console.WriteLine(HeistMemberOne.Name);
         }
     }
+    
 }
---
+++ Members.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHeist
+{
+    internal class Member1
+    {
+        internal string Name { get; set; }
+        internal int Skill { get; set; }
+        internal decimal CourageFactor { get; set; }
+        public Member1(string name, int skillLevel, decimal courageFactor)
+        {
+            this.Name = name;
+            this.Skill = skillLevel;
+            this.CourageFactor = courageFactor;
+        }
+    }
+
+    internal class Member2
+    {
+        internal string Name { get; set; }
+        internal int Skill { get; set; }
+        internal decimal CourageFactor { get; set; }
+        public Member2(string name, int skillLevel, decimal courageFactor)
+        {
+            this.Name = name;
+            this.Skill = skillLevel;
+            this.CourageFactor = courageFactor;
+        }
+    }
+    internal class Member3 
+    {
+        internal string Name { get; set; }
+        internal int Skill { get; set; }
+        internal decimal CourageFactor { get; set; }
+        public Member3(string name, int skillLevel, decimal courageFactor)
+        {
+            this.Name = name;
+            this.Skill = skillLevel;
+            this.CourageFactor = courageFactor;
+        }
+    }
+}
